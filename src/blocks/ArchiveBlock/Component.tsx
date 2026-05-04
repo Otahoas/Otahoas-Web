cedfed2d4844a0363d929a083f3d21d29ec52339
Mikko Juusti <mikko.juusti@windowslive.com>
2026-05-04 17:19:10 +0300

    Add locales to posts.

--- src/blocks/ArchiveBlock/Component.tsx
+++ src/blocks/ArchiveBlock/Component.tsx
@@ -1,4 +1,5 @@
 import type { Post, ArchiveBlock as ArchiveBlockProps } from '@/payload-types'
+import type { Locale } from '@/i18n/config'
 
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
@@ -10,9 +11,18 @@ import { CollectionArchive } from '@/components/CollectionArchive'
 export const ArchiveBlock: React.FC<
   ArchiveBlockProps & {
     id?: string
+    locale?: Locale
   }
 > = async (props) => {
-  const { id, categories, introContent, limit: limitFromProps, populateBy, selectedDocs } = props
+  const {
+    id,
+    categories,
+    introContent,
+    limit: limitFromProps,
+    locale,
+    populateBy,
+    selectedDocs,
+  } = props
 
   const limit = limitFromProps || 3
 
@@ -30,6 +40,7 @@ export const ArchiveBlock: React.FC<
       collection: 'posts',
       depth: 1,
       limit,
+      locale,
       ...(flattenedCategories && flattenedCategories.length > 0
         ? {
             where: {
@@ -59,7 +70,7 @@ export const ArchiveBlock: React.FC<
           <RichText className="ms-0 max-w-[48rem]" data={introContent} enableGutter={false} />
         </div>
       )}
-      <CollectionArchive posts={posts} />
+      <CollectionArchive posts={posts} locale={locale} />
     </div>
   )
 }
